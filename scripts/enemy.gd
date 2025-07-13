aba2c6f51f615f7502d633fafb2b1e98ac9dc042
joshuafolkken <joshuafolkken@gmail.com>
2025-07-13 11:50:44 +0900

    tread #20

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,28 @@
 extends CharacterBody2D
 
+@export var move_speed: int = 75
+@export var can_tread: bool = false
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	velocity.x = 75
+	velocity.x = move_speed
 
 	move_and_slide()
+
+
+func _is_treaded_by_character(body: Node2D) -> bool:
+	if not can_tread:
+		return false
+
+	if body.name != "Character":
+		return false
+
+	return true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if _is_treaded_by_character(body):
+		queue_free()
